f6eb96ea9b12a824212168d6e66df129fc89699c
Nathalia Molina Da Silva <112526050+Nathy019@users.noreply.github.com>
2023-05-12 21:03:38 -0300

    Adicionar arquivos de projeto.

---
+++ namor/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace namor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSim = new System.Windows.Forms.Button();
+            this.btnNao = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNao1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSim
+            // 
+            this.btnSim.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSim.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSim.Location = new System.Drawing.Point(12, 170);
+            this.btnSim.Name = "btnSim";
+            this.btnSim.Size = new System.Drawing.Size(61, 34);
+            this.btnSim.TabIndex = 0;
+            this.btnSim.Text = "Sim\r\n";
+            this.btnSim.UseVisualStyleBackColor = false;
+            this.btnSim.Click += new System.EventHandler(this.btnSim_Click);
+            // 
+            // btnNao
+            // 
+            this.btnNao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnNao.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNao.ForeColor = System.Drawing.Color.Black;
+            this.btnNao.Location = new System.Drawing.Point(315, 179);
+            this.btnNao.Name = "btnNao";
+            this.btnNao.Size = new System.Drawing.Size(107, 34);
+            this.btnNao.TabIndex = 1;
+            this.btnNao.Text = "Não";
+            this.btnNao.UseVisualStyleBackColor = false;
+            this.btnNao.Click += new System.EventHandler(this.button2_Click);
+            this.btnNao.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(336, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Quer casar comigo?";
+            // 
+            // btnNao1
+            // 
+            this.btnNao1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnNao1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnNao1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNao1.Location = new System.Drawing.Point(335, 265);
+            this.btnNao1.Name = "btnNao1";
+            this.btnNao1.Size = new System.Drawing.Size(107, 34);
+            this.btnNao1.TabIndex = 3;
+            this.btnNao1.Text = "Não";
+            this.btnNao1.UseVisualStyleBackColor = false;
+            this.btnNao1.Visible = false;
+            this.btnNao1.Click += new System.EventHandler(this.btnNao2_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(435, 99);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(107, 34);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "Não";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Visible = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(36, 296);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(107, 34);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "Não";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Visible = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(12, 12);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(107, 34);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "Não";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Visible = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(95, 170);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(107, 34);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "Não";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Visible = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btnNao1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNao);
+            this.Controls.Add(this.btnSim);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSim;
+        private System.Windows.Forms.Button btnNao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNao1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+    }
+}
+
---
+++ namor/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace namor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.btnNao.Visible = false;
+            this.btnNao1.Visible = true;
+            
+            
+        }
+
+        private void btnNao2_Click(object sender, EventArgs e)
+        {
+            this.btnNao.Visible = false;
+            this.btnNao1.Visible = false;
+            this.btn2.Visible = true;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            this.btnNao.Visible = false;
+            this.btnNao1.Visible = false;
+            this.btn2.Visible = false;
+            this.btn3.Visible = true;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            this.btnNao.Visible = false;
+            this.btnNao1.Visible = false;
+            this.btn2.Visible = false;
+            this.btn3.Visible = false;
+            this.btn4.Visible = true;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            this.btnNao.Visible = false;
+            this.btnNao1.Visible = false;
+            this.btn2.Visible = false;
+            this.btn3.Visible = false;
+            this.btn4.Visible = false;
+            this.btn5.Visible = true;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("POXA NÃO QUER CRIAR UMA VIDA DE CASADAS JUNTINHAS???", "TOXICA",MessageBoxButtons.OK);
+            Close();
+        }
+
+        private void btnSim_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("EBAAAAAA, ENTÃO VAMOS CRIAR UMA VIDA JUNTINHAS CHEIA DE GATINHOS E MTO AMORR!!!!!!", "LINDA TE AMOOOO", MessageBoxButtons.OK);
+            Close();
+        }
+    }
+}
